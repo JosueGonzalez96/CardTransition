baeea225447cf9d68cc7f4c075799beb4a656b0d
Alberto Josue Gonzalez Juarez <josue.gonzalez@globant.com>
2018-10-06 02:52:44 -0500

    first commit

---
+++ CardTransition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardTransition
+//
+//  Created by Alberto Josue Gonzalez Juarez on 06/10/18.
+//  Copyright © 2018 Alberto Josue Gonzalez Juarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
